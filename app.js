c6f10866a80e53eaa68d7b14865dbe5ec5f0558f
bankie606 <frcrescioni@gmail.com>
2016-06-01 17:42:20 +0000

    added pngs for trasparent coolness

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ introApp.controller('IntroController', ['$scope', function($scope) {
   $scope.name = 'User';
   $scope.buttonText = 'Reset';
   $scope.synths = [
-    {id: 0, brand: 'korg', model:'Poly-800', price: 250.00, polyphony: '6 Voices', src:'public/images/poly800.jpg'},
+    {id: 0, brand: 'korg', model:'Poly-800', price: 250.00, polyphony: '6 Voices', src:'public/images/poly800.png'},
     {id: 1, brand: 'nord', model:'Micro-Modular', price: 400.00, polyphony: 'Variable/ 6 Voices', src: 'public/images/micromodular.png'},
-    {id: 2, brand: 'elektron',model:'Monomachine', price: 500.00, polyphony: 'Variable/ 6 Voices', src: 'public/images/monomachine.jpg'},
-    {id: 3, brand: 'ensoniq', model:'Fizmo', price: 1500.00, polyphony: '32 Voices', src:'public/images/fizmo.jpeg'},
-    {id: 4, brand: 'moog', model:'Memory Moog', price: 2500.00, polyphony: 'No, monophonic', src:'public/images/memorymoog.jpeg'},
-    {id: 5, brand: 'buchla', model:'Who In Their Right Mind Would Buy This?', price: 6500.00, polyphony: "No, monophonic", src:'public/images/buchla.jpg'},
-    {id: 6, brand: 'emu' , model:"Proteus-1", price: 65.00, polyphony: '16 Voices', src:'public/images/proteus.jpg'}
+    {id: 2, brand: 'elektron',model:'Monomachine', price: 500.00, polyphony: 'Variable/ 6 Voices', src: 'public/images/monomachine.png'},
+    {id: 3, brand: 'ensoniq', model:'Fizmo', price: 1500.00, polyphony: '32 Voices', src:'public/images/fizmo.png'},
+    {id: 4, brand: 'moog', model:'Memory Moog', price: 2500.00, polyphony: 'No, monophonic', src:'public/images/memorymoog.png'},
+    {id: 5, brand: 'buchla', model:'Who In Their Right Mind Would Buy This?', price: 6500.00, polyphony: "No, monophonic", src:'public/images/buchla.png'},
+    {id: 6, brand: 'emu' , model:"Proteus-1", price: 65.00, polyphony: '16 Voices', src:'public/images/proteus.png'}
     ];
  
  
